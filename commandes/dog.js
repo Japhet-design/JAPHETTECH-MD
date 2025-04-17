6f9f500c2305bee5ff2076fa75162c0df492095d
Japhet <japhetisalingu@gmail.com>
2025-04-17 23:34:43 +0300

    Update dog.js

--- commandes/dog.js
+++ commandes/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'RAHMANI MD BRING 📂DOG PICS\n\n\n> *BY Rahmani Md*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'JAPHETTECH MD BRING 📂DOG PICS\n\n\n> *BY Japhettech Md*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
